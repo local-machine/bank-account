f3633ac9c481ee9cd784b888a591d9983014fb78
Jessica Munoz and Na Hyung Choi <jessica@>
2019-06-18 14:38:17 -0700

    build basic UI and BankAccount constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+function BankAccount(name, balance) {
+  this.name = name,
+  this.balance = balance
+}
+
 $(function () {
-  
+  $(".registerInput").submit(function(event) {
+    event.preventDefault();
+
+    var newName = $("#newName").val();
+    var newDeposit = parseInt($("#newDeposit").val());
+
+    var newAccount = new BankAccount(newName, newDeposit);
+
+    console.log(newAccount.balance);
+  })
 })
